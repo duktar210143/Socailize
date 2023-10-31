bba7a78c6c1fa23c95773e5f2128e169271a7670
duktartamang <duktar13@gmail.com>
2023-10-31 15:35:46 +0545

    Complete UI for SignUp page

---
+++ lib/widgets/text_form_field.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class textFormField extends StatelessWidget {
+  final String hintText;
+  final TextEditingController controller;
+  const textFormField({
+    Key? key,
+    required this.hintText,
+    required this.controller,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: TextFormField(
+        controller: controller,
+        style: const TextStyle(
+          color: Colors.white,
+          fontSize: 20
+        ),
+        decoration: InputDecoration(
+          hintStyle: const TextStyle(
+            color: Colors.white,
+            fontSize: 24,
+          ),
+          hintText: hintText,
+          border: OutlineInputBorder(
+            borderRadius:
+                BorderRadius.circular(80.0), // Adjust the border radius
+            borderSide: const BorderSide(
+              color: Colors.blue, // Change the border color
+              width: 2.0, // Change the border width
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}

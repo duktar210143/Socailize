9d6415b5b55fbbfdd88844f3ffbbc754bbd23162
duktartamang <duktar13@gmail.com>
2023-11-01 11:09:32 +0545

    Set up navigation routes in the application

---
+++ lib/app/app.dart
@@ -0,0 +1,15 @@
+import 'package:discussion_forum/app/authentication/signup_view.dart';
+import 'package:discussion_forum/app/routes/app_routes.dart';
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute: AppRoutes.signUpRoute,
+      routes: AppRoutes.getAppliactionRoute(),
+    );
+  }
+}
---
+++ lib/app/routes/app_routes.dart
@@ -0,0 +1,16 @@
+import 'package:discussion_forum/app/authentication/login_view.dart';
+import 'package:discussion_forum/app/authentication/signup_view.dart';
+
+class AppRoutes {
+  AppRoutes._();
+
+  static const String loginRoute = "/login";
+  static const String signUpRoute = "/signup";
+
+  static getAppliactionRoute() {
+    return {
+      signUpRoute: (context) => const SignUpView(),
+      loginRoute: (context) => const LoginView(),
+    };
+  }
+}

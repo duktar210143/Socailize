3480b50281dec45993b8761fdf02e6602aa5f6db
duktartamang <duktar13@gmail.com>
2023-11-08 13:58:14 +0545

    modifications in the login page

--- lib/app/authentication/login_view.dart
+++ lib/app/authentication/login_view.dart
@@ -1,13 +1,16 @@
+import 'package:discussion_forum/app/routes/app_routes.dart';
+import 'package:discussion_forum/providers/user_provider.dart';
 import 'package:discussion_forum/widgets/text_form_field.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:rive/rive.dart';
 
-class LoginView extends StatelessWidget {
+class LoginView extends ConsumerWidget {
   const LoginView({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     final FirebaseAuth auth = FirebaseAuth.instance;
     final GlobalKey<FormState> authKey = GlobalKey();
     final RegExp emailValid = RegExp(
@@ -70,10 +73,26 @@ class LoginView extends StatelessWidget {
                       onPressed: () async {
                         if (authKey.currentState!.validate()) {
                           try {
-                            await auth.signInWithEmailAndPassword(
-                              email: usernameController.text,
-                              password: passwordController.text,
-                            );
+                            // await auth.signInWithEmailAndPassword(
+                            //   email: usernameController.text,
+                            //   password: passwordController.text,
+                            // );
+                            // send an api request to check whether the user credentials exists
+                            final success = await ref
+                                .watch(userStateProvider.notifier)
+                                .login(usernameController.text,
+                                    passwordController.text);
+
+                            if (success) {
+                              Navigator.pushNamed(context, AppRoutes.homeRoute);
+                            } else {
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                const SnackBar(
+                                  content: Text('user with that username of password donot exist'),
+                                ),
+                              );
+                              Navigator.pushNamed(context, AppRoutes.loginRoute);
+                            }
                           } catch (e) {
                             ScaffoldMessenger.of(context).showSnackBar(
                               SnackBar(
@@ -91,6 +110,30 @@ class LoginView extends StatelessWidget {
                             fontWeight: FontWeight.bold),
                       ),
                     ),
+                  ),
+
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text(
+                        "Don't have an account ??",
+                        style: TextStyle(
+                            fontSize: 17, fontWeight: FontWeight.w500),
+                      ),
+                      TextButton(
+                        onPressed: () {
+                          Navigator.pushNamed(
+                            context,
+                            AppRoutes.signUpRoute,
+                          );
+                        },
+                        child: const Text(
+                          "SignUp",
+                          style: TextStyle(
+                              fontSize: 19, fontWeight: FontWeight.w500),
+                        ),
+                      ),
+                    ],
                   )
                 ],
               ),

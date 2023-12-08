ea0bafcdee70567a5a54a7673467c19c34124a70
duktartamang <duktar13@gmail.com>
2023-12-08 15:42:51 +0545

    added new fonts and theme to the application

---
+++ lib/theme/theme_data.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+ThemeData getApplicationTheme() {
+  return ThemeData(
+    primarySwatch: Colors.yellow,
+    scaffoldBackgroundColor: Colors.grey[200],
+    fontFamily: 'Monserrat Bold',
+  );
+}

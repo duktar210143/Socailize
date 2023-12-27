5caa1b185ffe6d881f1f13dcbc186cb5f09dc4fa
duktartamang <duktar13@gmail.com>
2023-12-27 12:23:44 +0545

    maintained clean architecture

---
+++ lib/app.dart
@@ -0,0 +1,35 @@
+import 'package:discussion_forum/features/authentication/login_view.dart';
+import 'package:discussion_forum/features/home/dashboard_view.dart';
+import 'package:discussion_forum/routes/app_routes.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute: AppRoutes.homeRoute, // Set the initial route
+      routes: AppRoutes.getAppliactionRoute(),
+      onGenerateInitialRoutes: (String initialRoute) {
+        return [
+          MaterialPageRoute(
+            builder: (context) {
+              return StreamBuilder<User?>(
+                stream: FirebaseAuth.instance.authStateChanges(),
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    return Text("DashBoard"); // Navigate to the home screen if the user is logged in.
+                  } else {
+                    return const LoginView(); // Navigate to the sign-up screen if the user is not logged in.
+                  }
+                },
+              );
+            },
+          ),
+        ];
+      },
+    );
+  }
+}
---
+++ lib/routes/app_routes.dart
@@ -0,0 +1,19 @@
+import 'package:discussion_forum/features/authentication/login_view.dart';
+import 'package:discussion_forum/features/authentication/signup_view.dart';
+import 'package:discussion_forum/features/home/dashboard_view.dart';
+
+class AppRoutes {
+  AppRoutes._();
+
+  static const String loginRoute = "/login";
+  static const String signUpRoute = "/signup";
+  static const String homeRoute = "/home";
+
+  static getAppliactionRoute() {
+    return {
+      signUpRoute: (context) => SignUpView(),
+      loginRoute: (context) => const LoginView(),
+      homeRoute: (context) => const DashboardView(),
+    };
+  }
+}
